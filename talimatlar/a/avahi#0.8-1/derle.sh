4b9d470e76eae9ddfcc14d85b425f3e8d39b5eec
Aylinux <cihanalk@gmail.com>
2021-01-20 17:51:19 +0300

    Update derle.sh

--- talimatlar/a/avahi#0.8-1/derle.sh
+++ talimatlar/a/avahi#0.8-1/derle.sh
@@ -1,13 +1,19 @@
-ekconf="--disable-qt3 --disable-qt4 --disable-mono --disable-monodoc
- --disable-doxygen-doc --enable-compat-libdns_sd --enable-compat-howl
- --with-xml=expat --with-avahi-user=avahi --with-avahi-group=avahi --with-autoipd-user=avahi
- --with-autoipd-group=avahi --with-distro=none --disable-xmltoman
- --disable-static ssp_cv_lib=no
- with_dbus_sys=/usr/share/dbus-1/system.d 
- --disable-python --disable-gtk
- --disable-gtk3 --sbindir=/usr/bin
- --with-systemdsystemunitdir=no"
+ NOCONFIGURE=1 ./autogen.sh
+ 
+  ./configure \
+    --prefix=/usr \
+    --sysconfdir=/etc \
+    --localstatedir=/var \
+    --sbindir=/usr/bin \
+    with_dbus_sys=/usr/share/dbus-1/system.d \
+    --disable-mono \
+    --enable-compat-libdns_sd \
+    --with-distro=none \
+    --with-avahi-priv-access-group=network \
+    --with-autoipd-user=avahi \
+    --with-autoipd-group=avahi \
+     --with-systemdsystemunitdir=no
 
-autoreconf -if
+  sed -i -e 's/ -shared / -Wl,-O1,--as-needed\0/g' libtool
 
-sed -e 's,\(avahi_runtime_dir=\).*,\1\"/run\",' -i configure
+  make
