2a16f355a4dd1e60da69041f87926131730b7f46
Aylinux <cihanalk@gmail.com>
2021-01-20 23:50:13 +0300

    Update derle.sh

--- talimatlar/o/openssl#1.1.1i-1/derle.sh
+++ talimatlar/o/openssl#1.1.1i-1/derle.sh
@@ -1,6 +1,4 @@
-./Configure --prefix=/tools \
---openssldir=/tools/etc/ssl \
---libdir=lib \
+./Configure --prefix=/usr --openssldir=/etc/ssl --libdir=lib \
 shared no-ssl3-method enable-ec_nistp_64_gcc_128 linux-x86_64 \
 "-Wa,--noexecstack ${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
 make depend
