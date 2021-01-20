b42a0d9601dac415462ff5f31f6fd4a424c0fc75
Aylinux <cihanalk@gmail.com>
2021-01-20 20:27:18 +0300

    Update derle.sh

--- talimatlar/u/util-linux#2.36.1-1/derle.sh
+++ talimatlar/u/util-linux#2.36.1-1/derle.sh
@@ -1,19 +1,18 @@
-mkdir -pv $PKG/var/lib/hwclock
-./configure ADJTIME_PATH=/var/lib/hwclock/adjtime \
---prefix=/usr \
---libdir=/usr/lib \
---bindir=/usr/bin \
---sbindir=/usr/bin \
---localstatedir=/var \
---enable-fs-paths-default=/usr/bin:/usr/local/bin \
---disable-chfn-chsh  \
---disable-login      \
---disable-nologin    \
---disable-su         \
---disable-setpriv    \
---disable-runuser    \
---disable-pylibmount \
---disable-static     \
+./configure \
+    --prefix=/usr \
+    --libdir=/usr/lib \
+    --bindir=/usr/bin \
+    --sbindir=/usr/bin \
+    --localstatedir=/var \
+    --enable-usrdir-path \
+    --enable-fs-paths-default=/usr/bin:/usr/local/bin \
+    --enable-raw \
+    --enable-vipw \
+    --enable-newgrp \
+    --enable-chfn-chsh \
+    --enable-write \
+    --enable-mesg \
+    --disable-hardlink \
 --without-python     \
 --without-systemd    \
 --without-systemdsystemunitdir
