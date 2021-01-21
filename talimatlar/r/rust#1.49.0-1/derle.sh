1a0ff8223a81f9aec727d3f1bd09bef7855ea75c
Aylinux <cihanalk@gmail.com>
2021-01-21 19:39:49 +0300

    Update derle.sh

--- talimatlar/r/rust#1.49.0-1/derle.sh
+++ talimatlar/r/rust#1.49.0-1/derle.sh
@@ -1,51 +1,55 @@
-cachedir="build/cache/2020-06-04"
-mkdir -p "$cachedir"
+ cd "rustc-$surum-src"
 
-cp -f /sources/rust-std-1.44.0-x86_64-unknown-linux-gnu.tar.xz $cachedir/
-cp -f /sources/rustc-1.44.0-x86_64-unknown-linux-gnu.tar.xz $cachedir/
-cp -f /sources/cargo-0.45.0-x86_64-unknown-linux-gnu.tar.xz $cachedir/
-
-# 'rust' checksums files in 'vendor/', but we patch a few.
-for vendor in libc openssl-sys; do
-    sed -i 's/\("files":{\)[^}]*/\1/' "vendor/$vendor/.cargo-checksum.json"
-done
-
-cat > config.toml <<EOF
+  cat >config.toml <<END
 [llvm]
 link-shared = true
-
 [build]
-build  = "x86_64-unknown-linux-gnu"
-host   = [ "x86_64-unknown-linux-gnu" ]
-target = [ "x86_64-unknown-linux-gnu" ]
-docs           = false
-compiler-docs  = false
-extended       = true
-submodules     = false
-python         = "python3"
-locked-deps    = true
-vendor         = true
-sanitizers     = false
-profiler       = false
-full-bootstrap = false
-
+target = ["x86_64-unknown-linux-gnu", "i686-unknown-linux-gnu", "x86_64-unknown-linux-musl"]
+tools = ["cargo", "rls", "clippy", "miri", "rustfmt", "analysis", "src"]
+cargo = "/usr/bin/cargo"
+rustc = "/usr/bin/rustc"
+python = "/usr/bin/python"
+extended = true
+sanitizers = false
+profiler = true
+vendor = true
 [install]
 prefix = "/usr"
-
 [rust]
-channel          = "stable"
-rpath            = false
-codegen-units    = 1
-debuginfo-level  = 0
-debug            = false
-backtrace        = false
-jemalloc         = false
-debug-assertions = false
-codegen-tests    = false
-
+# LLVM crashes when passing an object through ThinLTO twice.  This is triggered
+# when using rust code in cross-language LTO if libstd was built using ThinLTO.
+# http://blog.llvm.org/2019/09/closing-gap-cross-language-lto-between.html
+# https://github.com/rust-lang/rust/issues/54872
+codegen-units-std = 1
+debuginfo-level-std = 2
+channel = "stable"
+rpath = false
 [target.x86_64-unknown-linux-gnu]
 llvm-config = "/usr/bin/llvm-config"
-crt-static  = false
-EOF
+[target.x86_64-unknown-linux-musl]
+musl-root = "/usr/lib/musl"
+END
 
-LC_ALL=C python3 ./x.py build -j "$(nproc)"
+export RUST_BACKTRACE=1
+  export RUST_COMPILER_RT_ROOT="$SRC/compiler-rt-11.0.0.src"
+
+  python ./x.py dist -j "$(nproc)"
+  DESTDIR="$PWD"/dest-rust python ./x.py install -j "$(nproc)"
+
+  # Remove analysis data for libs that weren't installed
+  # TODO: Find out where these come from
+  local file lib
+  while read -rd '' file; do
+    lib="${file%.json}.rlib"
+    lib="${lib/\/analysis\///lib/}"
+    if [[ ! -e $lib ]]; then
+      echo "missing '$lib'"
+      rm -v "$file"
+    fi
+  done < <(find "dest-rust/usr/lib/rustlib"  -path '*/analysis/*.json' -print0)
+
+  # move docs and cross targets out of the way for splitting
+  mv dest-rust/usr/lib/rustlib/i686-unknown-linux-gnu dest-i686
+  mv dest-rust/usr/lib/rustlib/x86_64-unknown-linux-musl dest-musl
+  mv dest-rust/usr/share/doc dest-doc
+  
