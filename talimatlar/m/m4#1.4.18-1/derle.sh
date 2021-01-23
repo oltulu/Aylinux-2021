130902fb853374ddd8d0ab1e397d0517992d0ade
Aylinux <cihanalk@gmail.com>
2021-01-24 01:16:57 +0300

    Update derle.sh

--- talimatlar/m/m4#1.4.18-1/derle.sh
+++ talimatlar/m/m4#1.4.18-1/derle.sh
@@ -1,6 +1,6 @@
 unset CFLAGS
 sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
 echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
-./configure --prefix=/tools
+./configure --prefix=/usr
 make
 make -k check || true
