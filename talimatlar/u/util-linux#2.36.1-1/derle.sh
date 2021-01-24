bfdd2e4359ab05af9cbc0c65407cd47719b1b9f6
Aylinux <cihanalk@gmail.com>
2021-01-24 15:10:53 +0300

    Update derle.sh

--- talimatlar/u/util-linux#2.36.1-1/derle.sh
+++ talimatlar/u/util-linux#2.36.1-1/derle.sh
@@ -6,10 +6,12 @@
     --localstatedir=/var \
     --enable-usrdir-path \
     --enable-fs-paths-default=/usr/bin:/usr/local/bin \
+    --disable-{login,su,sulogin,nologin,runuser} \
+    --disable-chfn-chsh \
     --enable-raw \
+    --enable-kill \
     --enable-vipw \
     --enable-newgrp \
-    --enable-chfn-chsh \
     --enable-write \
     --enable-mesg \
     --disable-hardlink \
