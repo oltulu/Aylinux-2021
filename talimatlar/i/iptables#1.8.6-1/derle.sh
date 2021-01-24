a7a927a6b7461e75e70270129188a0a971956f87
Aylinux <cihanalk@gmail.com>
2021-01-24 14:55:18 +0300

    Update derle.sh

--- talimatlar/i/iptables#1.8.6-1/derle.sh
+++ talimatlar/i/iptables#1.8.6-1/derle.sh
@@ -1,9 +1,9 @@
 rm include/linux/types.h
 	
-./configure $confopt \
---enable-devel \
---enable-libipq \
---enable-shared 
+./configure --prefix=/usr \
+  --libexecdir=/usr/lib \
+  --enable-libipq \
+  --disable-nftables
 sed -e 's/ -shared / -Wl,-O1,--as-needed\0/g' -i libtool
 
 make
