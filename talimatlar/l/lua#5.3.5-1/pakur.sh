daee8812a7c72ac6c7b2f90d5f75ed503e24866f
Aylinux <cihanalk@gmail.com>
2021-01-24 03:13:27 +0300

    Update pakur.sh

--- talimatlar/l/lua#5.3.5-1/pakur.sh
+++ talimatlar/l/lua#5.3.5-1/pakur.sh
@@ -2,5 +2,5 @@ _majorver=${surum%.*}
 make \
 TO_LIB="liblua.a liblua.so liblua.so.$_majorver liblua.so.$surum" \
 INSTALL_DATA='cp -d' \
-INSTALL_TOP="$PKG"/tools \
+INSTALL_TOP="$PKG"/usr \
 install
