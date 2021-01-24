081f079055142d8b971f23ac1870c64c046d7ef0
Aylinux <cihanalk@gmail.com>
2021-01-24 15:04:05 +0300

    Update derle.sh

--- talimatlar/r/rsync#3.2.3-1/derle.sh
+++ talimatlar/r/rsync#3.2.3-1/derle.sh
@@ -1,5 +1,8 @@
-./configure $confopt \
---with-included-popt=no \
---with-included-zlib=yes \
---disable-debug
+ ./configure --prefix=/usr \
+                --with-rsh=ssh \
+                --with-included-zlib=no \
+                --with-included-popt=yes \
+                --disable-xxhash \
+                --disable-lz4 \
+                --disable-zstd
 make 
