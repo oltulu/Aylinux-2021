a8b560baace74273b71af2e88307d1b7db09e7c0
Aylinux <cihanalk@gmail.com>
2021-01-25 22:43:49 +0300

    Delete talimatlar/x/xorg-server#1.20.5-1 directory

--- talimatlar/x/xorg-server#1.20.5-1/derle.sh
+++
@@ -1,8 +0,0 @@
-./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var \
---with-xkb-output=/var/lib/xkb \
---enable-glamor \
---with-fontrootdir=/usr/share/fonts \
---enable-install-setuid \
---disable-systemd-logind \
---enable-suid-wrapper
-make
--- talimatlar/x/xorg-server#1.20.5-1/pakur.sh
+++
@@ -1,5 +0,0 @@
-make DESTDIR=$PKG install
-rm -rf $PKG/var
-rm $PKG/usr/lib/xorg/modules/{libexa,libfb,libfbdevhw,libglamoregl,libint10,libshadow,libshadowfb,libvbe,libvgahw,libwfb}.la
-rm $PKG/usr/lib/xorg/modules/drivers/modesetting_drv.la
-rm $PKG/usr/lib/xorg/modules/extensions/libglx.la
