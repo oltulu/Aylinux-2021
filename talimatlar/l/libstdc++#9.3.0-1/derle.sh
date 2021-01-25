84bbbb58546c533c4a72188b854b92f78836d63a
Aylinux <cihanalk@gmail.com>
2021-01-25 23:50:54 +0300

    Update derle.sh

--- talimatlar/l/libstdc++#9.3.0-1/derle.sh
+++ talimatlar/l/libstdc++#9.3.0-1/derle.sh
@@ -3,11 +3,11 @@ cd build
 
 ../libstdc++-v3/configure       \
 --host=$ONSISTEM_TARGET         \
---prefix=/tools                 \
+--prefix=/usr                 \
 --disable-multilib              \
 --disable-nls                   \
 --disable-libstdcxx-threads     \
 --disable-libstdcxx-pch         \
---with-gxx-include-dir=/tools/$ONSISTEM_TARGET/include/c++/$surum
+--with-gxx-include-dir=/usr/$ONSISTEM_TARGET/include/c++/$surum
 
 make
