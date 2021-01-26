146f238e42a636507a3e165f03e5a9bdfb800845
Aylinux <cihanalk@gmail.com>
2021-01-26 19:00:27 +0300

    Update derle.sh

--- talimatlar/a/avahi#0.8-1/derle.sh
+++ talimatlar/a/avahi#0.8-1/derle.sh
@@ -2,6 +2,7 @@ patch -Np1 -i /sources/reverse-move-to-run.patch
   NOCONFIGURE=1 ./autogen.sh
   ./configure --prefix=/usr \
     --sysconfdir=/etc \
+    --sbindir=/usr/bin \
     --localstatedir=/var \
     --with-distro=none \
     --disable-{gtk,gtk3,qt3,qt4,qt5,python} \
