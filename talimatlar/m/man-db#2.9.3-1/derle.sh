f3dbade076e8f6b965f71875d4859045bbeec49a
Aylinux <cihanalk@gmail.com>
2021-01-27 20:20:35 +0300

    Update derle.sh

--- talimatlar/m/man-db#2.9.3-1/derle.sh
+++ talimatlar/m/man-db#2.9.3-1/derle.sh
@@ -1,12 +1,10 @@
-./configure $confopt \
---disable-setuid          \
---enable-cache-owner=root \
---docdir=/usr/share/doc/man-db-2.8.5 \
---with-db=gdbm \
---with-browser=/usr/bin/lynx   \
---with-vgrind=/usr/bin/vgrind  \
---with-grap=/usr/bin/grap \
---with-systemdtmpfilesdir=           \
---with-systemdsystemunitdir=
+./configure --prefix=/usr \
+        --sysconfdir=/etc \
+        --libexecdir=/usr/lib/$name \
+        --disable-setuid \
+        --disable-cache-owner \
+        --disable-nls \
+        --with-systemdtmpfilesdir=no \
+        --with-systemdsystemunitdir=no
 
 make
