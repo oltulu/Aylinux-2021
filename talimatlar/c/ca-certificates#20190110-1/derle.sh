5b87b04841f2d9ca18929e8174bc723d68b52317
Aylinux <cihanalk@gmail.com>
2021-01-30 01:32:14 +0300

    Update derle.sh

--- talimatlar/c/ca-certificates#20190110-1/derle.sh
+++ talimatlar/c/ca-certificates#20190110-1/derle.sh
@@ -1,4 +1,5 @@
 gcc $CFLAGS ${SRC}/certdata2pem.c -o mozilla/certdata2pem
+chmod +x ${SRC}/remove-expired-certs.sh
 cp ${SRC}/remove-expired-certs.sh mozilla
 sed -i mozilla/Makefile -e 's,python certdata2pem.py,./certdata2pem,g'
 sed -i mozilla/Makefile -e "s;\(.*\)\(certdata2pem.*\);\1\2\n\1./remove-expired-certs.sh;"
