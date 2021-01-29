7fbc1da83b86a0cc294136cb99f76b966981831d
Aylinux <cihanalk@gmail.com>
2021-01-29 19:12:24 +0300

    Update derle.sh

--- talimatlar/k/kernel5910#5.9.10-1/derle.sh
+++ talimatlar/k/kernel5910#5.9.10-1/derle.sh
@@ -5,8 +5,8 @@
   # standart config
   cat "${SRC}/kernel-${bsurum}-config" > ./.config
   
-  export KBUILD_BUILD_USER="milisarge"
-  export KBUILD_BUILD_HOST="mls.akdeniz.edu.tr"
+  export KBUILD_BUILD_USER="Cihan_Alkan"
+  export KBUILD_BUILD_HOST="aylinux.kripto.com.tr"
   
   # ayarları onaylama
   make olddefconfig
