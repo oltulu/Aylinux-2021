f40fff38658928bfc394be9167e4de80fdf4a37b
Aylinux <cihanalk@gmail.com>
2021-01-31 03:09:20 +0300

    Update derle.sh

--- talimatlar/u/util-linux#2.36.1-1/derle.sh
+++ talimatlar/u/util-linux#2.36.1-1/derle.sh
@@ -1,20 +1,9 @@
-./configure \
-    --prefix=/usr \
-    --libdir=/usr/lib \
-    --bindir=/usr/bin \
-    --sbindir=/usr/bin \
-    --localstatedir=/var \
-    --enable-usrdir-path \
-    --enable-fs-paths-default=/usr/bin:/usr/local/bin \
-    --disable-{login,su,sulogin,nologin,runuser} \
-    --disable-chfn-chsh \
-    --enable-raw \
-    --enable-kill \
-    --enable-vipw \
-    --enable-newgrp \
-    --enable-write \
-    --enable-mesg \
-    --disable-hardlink \
+
+    ./configure --prefix=/usr \
+                --disable-nls \
+                --disable-{login,su,sulogin,nologin,runuser} \
+                --disable-chfn-chsh \
+                --enable-{write,raw,kill} \
 --without-python     \
 --without-systemd    \
 --without-systemdsystemunitdir
