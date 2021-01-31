b5639fbcab52fb6019b090e1091a7699775bdaa2
Aylinux <cihanalk@gmail.com>
2021-01-31 16:38:56 +0300

    Update derle.sh

--- talimatlar/g/grub#2.04-1/derle.sh
+++ talimatlar/g/grub#2.04-1/derle.sh
@@ -1,21 +1,8 @@
-sed -i -e 's,freetype/ftsynth.h,freetype2/ftsynth.h,' util/grub-mkfont.c
-
-sed 's|GNU/Linux|Linux|' -i "util/grub.d/10_linux.in"
-
-unset CFLAGS
-unset CPPFLAGS
-unset CXXFLAGS
-unset LDFLAGS
-unset MAKEFLAGS
-
-common_confs+="--enable-device-mapper --enable-cache-stats --enable-nls
---enable-grub-mkfont --enable-grub-mount --disable-werror --enable-boot-time"
-
-# fix unifont.bdf location so grub-mkfont can create *.pf2 files
-sed -i 's|/usr/share/fonts/unifont|/usr/share/fonts/misc|' configure
+export CFLAGS="${CFLAGS} -Wno-error"
+export PYTHON=python3
 
 cp -r "${SRC}/grub-$surum/" ${SRC}/grub-efi
-
+cd ${SRC}/grub-$surum
 ./configure --prefix=/usr \
 		--sysconfdir=/etc \
 		--enable-device-mapper \
