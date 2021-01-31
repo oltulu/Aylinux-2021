2d1f575762418b8b2c58f915af0bbf665fa442af
Aylinux <cihanalk@gmail.com>
2021-01-31 22:52:11 +0300

    Update derle.sh

--- talimatlar/e/expect#5.45.4-1/derle.sh
+++ talimatlar/e/expect#5.45.4-1/derle.sh
@@ -2,8 +2,8 @@ cd expect${surum}
 cp -v configure{,.orig}
 sed 's:/usr/local/bin:/bin:' configure.orig > configure
 ./configure \
---prefix=/tools \
---with-tcl=/tools/lib \
---with-tclinclude=/tools/include
+--prefix=/usr \
+--with-tcl=/usr/lib \
+--with-tclinclude=/usr/include
 make
 make test
