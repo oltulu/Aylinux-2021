3420bae027d4676b46f5ce6209da3b9e673a4486
Aylinux <cihanalk@gmail.com>
2021-02-03 20:30:45 +0300

    Delete talimatlar/e/e2fsprogs#1.45.6-1 directory

--- talimatlar/e/e2fsprogs#1.45.6-1/pakur.sh
+++
@@ -1,9 +0,0 @@
-make -j1 DESTDIR="${PKG}" install install-libs
-	
-sed -i -e 's/^AWK=.*/AWK=awk/' "${PKG}/usr/bin/compile_et"
-
-# derleme dizini referansları temizle
-sed -i -e 's#^SS_DIR=.*#SS_DIR="/usr/share/ss"#' "${PKG}/usr/bin/mk_cmds"
-sed -i -e 's#^ET_DIR=.*#ET_DIR="/usr/share/et"#' "${PKG}/usr/bin/compile_et"
-
-rm "${PKG}"/usr/lib/lib{com_err,e2p,ext2fs,ss}.a
