f3652d2808bc356ef616e5b662cd7ca6372dd80c
Aylinux <cihanalk@gmail.com>
2021-02-03 22:01:47 +0300

    Update derle.sh

--- talimatlar/u/util-linux#2.36.1-1/derle.sh
+++ talimatlar/u/util-linux#2.36.1-1/derle.sh
@@ -1,5 +1,12 @@
 
-    ./configure --prefix=/usr \
+    ./configure \
+    --prefix=/usr \
+    --libdir=/usr/lib \
+    --bindir=/usr/bin \
+    --sbindir=/usr/bin \
+    --localstatedir=/var \
+    --enable-usrdir-path \
+    --enable-fs-paths-default=/usr/bin:/usr/local/bin \
                 --disable-nls \
                 --disable-{login,su,sulogin,nologin,runuser} \
                 --disable-chfn-chsh \
