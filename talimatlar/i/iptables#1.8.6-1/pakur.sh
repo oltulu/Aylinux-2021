343d6ba948030dcfda3e75fd85057d49767b67e7
Aylinux <cihanalk@gmail.com>
2021-02-05 18:51:32 +0300

    Delete talimatlar/i/iptables#1.8.6-1 directory

--- talimatlar/i/iptables#1.8.6-1/pakur.sh
+++
@@ -1,7 +0,0 @@
-make DESTDIR=$PKG install
-	
-cd ${SRC}
-install -Dm644 empty.rules "$PKG/etc/iptables/iptables.rules"
-install -Dm644 empty.rules "$PKG/etc/iptables/ip6tables.rules"
-install -Dt "$PKG/usr/share/iptables" -m644 *.rules
-ln -srt "$PKG/etc/iptables" "$PKG"/usr/share/iptables/{empty,simple_firewall}.rules
