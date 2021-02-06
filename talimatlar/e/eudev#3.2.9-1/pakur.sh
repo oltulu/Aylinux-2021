902257230ef1e48861333c3d3347db443525d23e
Aylinux <cihanalk@gmail.com>
2021-02-07 00:29:16 +0300

    Update pakur.sh

--- talimatlar/e/eudev#3.2.9-1/pakur.sh
+++ talimatlar/e/eudev#3.2.9-1/pakur.sh
@@ -1,23 +1,18 @@
 mkdir -pv $PKG/usr/lib/{firmware,udev/devices/pts,udev/devices/shm}
 	
 mkdir -pv $PKG/usr/lib/udev/rules.d
-mkdir -pv $PKG/etc/udev/rules.d
+make CFLAGS="$CFLAGS -D_GNU_SOURCE"
+    make install DESTDIR=$PKG
+    make -C man install DESTDIR=$PKG
 
-	make DESTDIR=$PKG install
-
-# man sayfalar
-pushd man
-make DESTDIR=$PKG install-man7 install-man8
-
-cd $SRC
-make -j1 -f udev-lfs-20171102/Makefile.lfs DESTDIR=$PKG install
+ # create binary hwdb
+    LD_LIBRARY_PATH=$PKG/usr/lib $PKG/usr/bin/udevadm hwdb --update --root=$PKG
 
 
-	$PKG/usr/bin/udevadm hwdb --update --root=$PKG
-rm -f $PKG/etc/udev/hwdb.bin
-rm -rf $PKG/usr/share/doc
-rm -rf $PKG/usr/lib/*.la
-mkdir -p $PKG/usr/lib/udev/keymaps/force-release
-rm -rf $PKG/lib/udev/rules.d
-mv $PKG/lib/udev/* $PKG/usr/lib/udev/
-rm -rf $PKG/lib
+    # Devices
+    install -d $PKG/usr/lib/{firmware,udev/devices/{pts,shm}}
+    install -d $PKG/usr/{lib,run}
+
+    # Add CRUX items
+ #   install -m 0755 $SRC/start_udev $PKG/usr/bin
+ #   install -m 0644 $SRC/81-crux.rules $PKG/usr/lib/udev/rules.d
