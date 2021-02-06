13ef86348af7aaa9f4c328b080e4b7f8e2725513
Aylinux <cihanalk@gmail.com>
2021-02-07 00:04:08 +0300

    Update derle.sh

--- talimatlar/l/lvm2#2.02.187-1/derle.sh
+++ talimatlar/l/lvm2#2.02.187-1/derle.sh
@@ -1,8 +1,14 @@
-./configure $confopt \
---prefix=/usr --sbindir=/usr/bin --sysconfdir=/etc --localstatedir=/var --enable-applib \
---enable-cmdlib \
---enable-pkgconfig \
---enable-udev_sync \
---enable-dmeventd \
---with-udev-prefix=/usr
+    CONFIG_SHELL=/bin/bash \
+    ./configure --prefix=/usr \
+                --exec-prefix= \
+                --datarootdir=/usr \
+                --mandir=/usr/share/man \
+                --with-udev-prefix= \
+                --disable-readline \
+                --disable-selinux \
+                --enable-pkgconfig \
+                --enable-static_link \
+                --enable-udev_sync \
+                --enable-udev_rules \
+                --with-{user,group}=root
 make
