b615240bc24c7a383934dbcf43cfbd9bd5f84830
Aylinux <cihanalk@gmail.com>
2021-02-07 00:23:06 +0300

    Update derle.sh

--- talimatlar/e/eudev#3.2.9-1/derle.sh
+++ talimatlar/e/eudev#3.2.9-1/derle.sh
@@ -1,21 +1,20 @@
-sed -r -i 's|/usr(/bin/test)|\1|' test/udev-test.pl
+sed -i '/^Libs:/s/-ludev/-ludev -lrt/' src/libudev/libudev.pc.in
 
-sed -e 's/GROUP="dialout"/GROUP="uucp"/' \
-		-e 's/GROUP="tape"/GROUP="storage"/' \
-		-e 's/GROUP="cdrom"/GROUP="optical"/' \
-		-i rules/*.rules
 
 	./configure --prefix=/usr \
         --sysconfdir=/etc \
 	--libdir=/usr/lib \
 	--sbindir=/usr/bin \
 	--with-rootprefix=      \
+	--libexecdir=/usr/lib \
 	--with-rootlibdir=/usr/lib  \
-	--enable-manpages       \
+	--disable-manpages       \
 	--disable-static        \
 	--config-cache \
 	--enable-kmod \
 	--disable-gtk-doc-html \
-	--with-firmware-path=/usr/lib/firmware
+	--with-firmware-path=/usr/lib/firmware  \
+        --disable-introspection \
+        --enable-split-usr
  make
 
