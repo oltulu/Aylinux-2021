73d4de94b4c8497e5bed0d4da4c7ae6061f8c714
Aylinux <cihanalk@gmail.com>
2021-02-19 19:58:06 +0300

    Create kurkos.sh

---
+++ talimatlar/p/papirus-icon-theme#20190817-1/kurkos.sh
@@ -0,0 +1,4 @@
+gtk-update-icon-cache /usr/share/icons/Papirus/
+gtk-update-icon-cache /usr/share/icons/Papirus-Dark/
+gtk-update-icon-cache /usr/share/icons/Papirus-Light/
+gtk-update-icon-cache /usr/share/icons/ePapirus/
