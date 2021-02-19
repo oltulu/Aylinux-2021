9dd571a953c97a61b3681be155d40609fdccc004
Aylinux <cihanalk@gmail.com>
2021-02-20 02:42:53 +0300

    Update derle.sh

--- talimatlar/v/vlc#3.0.11.1-1/derle.sh
+++ talimatlar/v/vlc#3.0.11.1-1/derle.sh
@@ -1,26 +1,25 @@
 sed -e 's:truetype/ttf-dejavu:TTF:g' -i modules/visualization/projectm.cpp
 sed -e 's|-Werror-implicit-function-declaration||g' -i configure
 sed 's|whoami|echo builduser|g' -i configure
-sed 's|hostname -f|echo milis|g' -i configure
+sed 's|hostname -f|echo aylinux|g' -i configure
 
 patch -Np1 < $SRC/vlc_qt_5_15.patch
 patch -Np1 < $SRC/vlc-3.0.11.1-srt_1.4.2.patch
 patch -Np1 < $SRC/vlc-lua53_compat.patch
 
 export CFLAGS+=" -I/usr/include/samba-4.0"
-export CPPFLAGS+=" -I/usr/include/samba-4.0"
-export CXXFLAGS+=" -std=c++11"
-export LUAC=/usr/bin/luac
-export LUA_LIBS="$(pkg-config --libs lua)"
-export RCC=/usr/bin/rcc-qt5
-export BUILDCC=gcc
+  export CPPFLAGS+=" -I/usr/include/samba-4.0"
+  export CXXFLAGS+=" -std=c++11"
+  export LUAC=/usr/bin/luac
+  export LUA_LIBS="$(pkg-config --libs lua)"
+  export RCC=/usr/bin/rcc-qt5
 
 ./configure --prefix=/usr \
 --sysconfdir=/etc \
 --disable-rpath \
 --enable-nls \
 --enable-archive \
---enable-live555 \
+--disable-live555 \
 --enable-dc1394 \
 --enable-dv1394 \
 --enable-dvdread \
