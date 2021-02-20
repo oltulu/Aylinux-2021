05b1c4f8c43bbb215733cb8735cb2c7aae95ef6e
Aylinux <cihanalk@gmail.com>
2021-02-20 14:06:02 +0300

    Update koskur.sh

--- talimatlar/g/gst-plugins-others#1.18.3-1/koskur.sh
+++ talimatlar/g/gst-plugins-others#1.18.3-1/koskur.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [ -d /var/lib/mps/db/gst-plugins-bad ]  && mps sil gst-plugins-bad --ona
-[ -d /var/lib/mps/db/gst-plugins-good ] && mps sil gst-plugins-good --ona
-[ -d /var/lib/mps/db/gst-plugins-ugly ] && mps sil gst-plugins-ugly --ona
+#[ -d /var/lib/mps/db/gst-plugins-good ] && mps sil gst-plugins-good --ona
+#[ -d /var/lib/mps/db/gst-plugins-ugly ] && mps sil gst-plugins-ugly --ona
 [ -d /var/lib/mps/db/gst-libav ]  &&  mps sil gst-libav --ona
 [ -d /var/lib/mps/db/gst-python ] &&  mps sil gst-python --ona
