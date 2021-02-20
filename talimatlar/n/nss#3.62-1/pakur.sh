a3a81f718fb697c6e21e490cc56ec9bf938355a1
Aylinux <cihanalk@gmail.com>
2021-02-20 14:39:58 +0300

    Update pakur.sh

--- talimatlar/n/nss#3.62-1/pakur.sh
+++ talimatlar/n/nss#3.62-1/pakur.sh
@@ -13,3 +13,30 @@ cd $SRC/nss-$surum
   ln -s nss.pc "$PKG$libdir/pkgconfig/mozilla-nss.pc"
 
   install -Dt "$PKG$libdir" dist/Release/lib/*.so
+  
+  cd $SRC/nss-$surum
+elma vmajor vminor vpatch
+  { read vmajor; read vminor; read vpatch; } \
+    < <(awk '/#define.*NSS_V(MAJOR|MINOR|PATCH)/ {print $3}' nss/lib/nss/nss.h)
+
+  sed nss/pkg/pkg-config/nss-config.in \
+    -e "s,@libdir@,$libdir,g" \
+    -e "s,@prefix@,/usr/bin,g" \
+    -e "s,@exec_prefix@,/usr/bin,g" \
+    -e "s,@includedir@,/usr/include/nss,g" \
+    -e "s,@MOD_MAJOR_VERSION@,$vmajor,g" \
+    -e "s,@MOD_MINOR_VERSION@,$vminor,g" \
+    -e "s,@MOD_PATCH_VERSION@,$vpatch,g" |
+    install -D /dev/stdin "$PKG/usr/bin/nss-config"
+
+  install -Dt "$PKG/usr/bin" \
+    dist/Release/bin/{*util,shlibsign,signtool,signver,ssltap}
+
+  install -Dt "$PKG/usr/include/nss" -m644 dist/public/nss/*.h
+
+  # Replace built-in trust with p11-kit connection
+  ln -s pkcs11/p11-kit-trust.so "$PKG$libdir/p11-kit-trust.so"
+  ln -sf p11-kit-trust.so "$PKG$libdir/libnssckbi.so"
+  
+  install -Dm644 ca-bundle.trust.p11-kit \
+    "$PKG/usr/share/ca-certificates/trust-source/mozilla.trust.p11-kit"
