77947f4acc0e665e734421c96b31b3a38b3b86f4
Aylinux <cihanalk@gmail.com>
2021-02-20 13:01:09 +0300

    Update pakur.sh

--- talimatlar/g/gst-plugins-others#1.18.3-1/pakur.sh
+++ talimatlar/g/gst-plugins-others#1.18.3-1/pakur.sh
@@ -1,11 +1,11 @@
-cd $SRC/gst-plugins-good-$surum
-DESTDIR=$PKG meson install -C build
+#cd $SRC/gst-plugins-good-$surum
+#DESTDIR=$PKG meson install -C build
 
 cd $SRC/gst-plugins-bad-$surum
 DESTDIR=$PKG meson install -C build
 
-cd $SRC/gst-plugins-ugly-$surum
-DESTDIR=$PKG meson install -C build
+#cd $SRC/gst-plugins-ugly-$surum
+#DESTDIR=$PKG meson install -C build
 
 cd $SRC/gst-libav-$surum
 DESTDIR=$PKG meson install -C build
