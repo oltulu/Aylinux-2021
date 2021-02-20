daf3b07a4c2821c156b2cf0bffbbf732b66d6935
Aylinux <cihanalk@gmail.com>
2021-02-20 14:50:40 +0300

    Update pakur.sh

--- talimatlar/n/nss#3.62-1/pakur.sh
+++ talimatlar/n/nss#3.62-1/pakur.sh
@@ -1,26 +1,26 @@
 cd $SRC/nss-$surum
 
-  local libdir=/usr/lib nsprver="$(pkg-config --modversion nspr)"
+#  nsprver="$(pkg-config --modversion nspr)"
   sed nss/pkg/pkg-config/nss.pc.in \
-    -e "s,%libdir%,$libdir,g" \
+    -e "s,%libdir%,/usr/lib,g" \
     -e "s,%prefix%,/usr,g" \
     -e "s,%exec_prefix%,/usr/bin,g" \
     -e "s,%includedir%,/usr/include/nss,g" \
-    -e "s,%NSPR_VERSION%,$nsprver,g" \
+    -e "s,%NSPR_VERSION%,$(pkg-config --modversion nspr),g" \
     -e "s,%NSS_VERSION%,$surum,g" |
-    install -Dm644 /dev/stdin "$PKG$libdir/pkgconfig/nss.pc"
+    install -Dm644 /dev/stdin "$PKG/usr/lib/pkgconfig/nss.pc"
 
-  ln -s nss.pc "$PKG$libdir/pkgconfig/mozilla-nss.pc"
+  ln -s nss.pc "$PKG/usr/lib/pkgconfig/mozilla-nss.pc"
 
-  install -Dt "$PKG$libdir" dist/Release/lib/*.so
+  install -Dt "$PKG/usr/lib" dist/Release/lib/*.so
   
   cd $SRC/nss-$surum
-elma vmajor vminor vpatch
+local vmajor vminor vpatch
   { read vmajor; read vminor; read vpatch; } \
     < <(awk '/#define.*NSS_V(MAJOR|MINOR|PATCH)/ {print $3}' nss/lib/nss/nss.h)
 
   sed nss/pkg/pkg-config/nss-config.in \
-    -e "s,@libdir@,$libdir,g" \
+    -e "s,@libdir@,/usr/lib,g" \
     -e "s,@prefix@,/usr/bin,g" \
     -e "s,@exec_prefix@,/usr/bin,g" \
     -e "s,@includedir@,/usr/include/nss,g" \
@@ -35,8 +35,8 @@ elma vmajor vminor vpatch
   install -Dt "$PKG/usr/include/nss" -m644 dist/public/nss/*.h
 
   # Replace built-in trust with p11-kit connection
-  ln -s pkcs11/p11-kit-trust.so "$PKG$libdir/p11-kit-trust.so"
-  ln -sf p11-kit-trust.so "$PKG$libdir/libnssckbi.so"
+  ln -s pkcs11/p11-kit-trust.so "$PKG/usr/lib/p11-kit-trust.so"
+  ln -sf p11-kit-trust.so "$PKG/usr/lib/libnssckbi.so"
   
   install -Dm644 ca-bundle.trust.p11-kit \
     "$PKG/usr/share/ca-certificates/trust-source/mozilla.trust.p11-kit"
