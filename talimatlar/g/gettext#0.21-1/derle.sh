702b7c686a7c92f9ee6f27680f26acee3dadfc1c
Aylinux <cihanalk@gmail.com>
2021-02-25 23:12:23 +0300

    Update derle.sh

--- talimatlar/g/gettext#0.21-1/derle.sh
+++ talimatlar/g/gettext#0.21-1/derle.sh
@@ -1,5 +1,5 @@
 cd gettext-tools
-EMACS="no" ./configure --prefix=/tools --disable-shared
+EMACS="no" ./configure --prefix=/usr --disable-shared
 make -C gnulib-lib
 make -C intl pluralx.c
 make -C src msgfmt
