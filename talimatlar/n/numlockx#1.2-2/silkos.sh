db85acb7af5669d61bfc84803a47ca0168cada74
Aylinux <cihanalk@gmail.com>
2021-02-28 02:43:36 +0300

    Update silkos.sh

--- talimatlar/n/numlockx#1.2-2/silkos.sh
+++ talimatlar/n/numlockx#1.2-2/silkos.sh
@@ -1,2 +1,4 @@
+if [ -f /etc/lightdm/lightdm.conf ];then
 echo "/etc/lightdm/lightdm.conf dosyası varsayılan haline döndürüldü"
 sed -i 's/greeter-setup-script=numlockx on/#greeter-setup-script=/g' /etc/lightdm/lightdm.conf
+fi
