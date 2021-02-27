1446e4fcb923e3a676d5dcac6e20e8abbdcecb55
Aylinux <cihanalk@gmail.com>
2021-02-28 02:42:57 +0300

    Update kurkos.sh

--- talimatlar/n/numlockx#1.2-2/kurkos.sh
+++ talimatlar/n/numlockx#1.2-2/kurkos.sh
@@ -1,2 +1,4 @@
+if [ -f /etc/lightdm/lightdm.conf ];then
 echo "/etc/lightdm/lightdm.conf dosyası değiştirildi"
 sed -i 's/#greeter-setup-script=/greeter-setup-script=numlockx on/g' /etc/lightdm/lightdm.conf
+fi
