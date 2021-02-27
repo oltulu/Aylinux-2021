f81153dfaaf102058e73e9b1828a3e080b011f84
Aylinux <cihanalk@gmail.com>
2021-02-27 13:23:11 +0300

    Delete derle.sh

--- talimatlar/g/git#2.30.0-1/derle.sh
+++
@@ -1,9 +0,0 @@
-make \
-prefix='/usr' \
-gitexecdir='/usr/lib/git-core' \
-perllibdir="$(/usr/bin/perl -MConfig -wle 'print $Config{installvendorlib}')" \
-CFLAGS="$CFLAGS" \
-LDFLAGS="$LDFLAGS" \
-NO_CROSS_DIRECTORY_HARDLINKS=1 \
-NO_PERL_CPAN_FALLBACKS=1 \
-USE_LIBPCRE2=1
