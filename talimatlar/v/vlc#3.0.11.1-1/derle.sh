fe26ecabc93fab6c4ba54676da76e687801586c5
Aylinux <cihanalk@gmail.com>
2021-03-03 22:22:29 +0300

    Update derle.sh

--- talimatlar/v/vlc#3.0.11.1-1/derle.sh
+++ talimatlar/v/vlc#3.0.11.1-1/derle.sh
@@ -1,22 +1,106 @@
-[[ -z $(pkg-config --libs --static libavutil | grep -- '-lvdpau') ]] && \
-    PKGMK_VLC+=' --disable-vdpau'
-
-  autoreconf -fi
-
-#  export LUAC=/usr/bin/luac5.2
-#  export LUA_LIBS="$(pkg-config --libs lua5.2)"
+export CFLAGS+=" -I/usr/include/samba-4.0"
+  export CPPFLAGS+=" -I/usr/include/samba-4.0"
+  export CXXFLAGS+=" -std=c++11"
+  # upstream doesn't support lua 5.4 yet: https://trac.videolan.org/vlc/ticket/25036
+  export LUAC=/usr/bin/luac5.2
+  export LUA_LIBS="$(pkg-config --libs lua5.2)"
   export RCC=/usr/bin/rcc-qt5
-  eval PKGMK_VLC="($PKGMK_VLC)"
 
-  sed -e 's|-Werror-implicit-function-declaration||g' -i configure
-
-  BUILDCC="gcc -std=gnu11"          \
-  ./configure "${PKGMK_VLC[@]}"     \
-              --prefix=/usr         \
-              --disable-nls         \
-              --enable-alsa         \
-              --disable-dbus        \
-              --disable-fribidi     \
-              --disable-update-check 
-
-  V=1 ./compile
+  ./configure \
+    --prefix=/usr \
+    --sysconfdir=/etc \
+    --with-kde-solid=/usr/share/solid/actions/ \
+    --disable-rpath \
+    --enable-nls \
+    --enable-archive \
+    --enable-live555 \
+    --enable-dc1394 \
+    --enable-dv1394 \
+    --enable-dvdread \
+    --enable-dvdnav \
+    --enable-bluray \
+    --disable-opencv \
+    --enable-smbclient \
+    --enable-sftp \
+    --enable-nfs \
+    --enable-realrtsp \
+    --enable-dvbpsi \
+    --enable-gme \
+    --enable-ogg \
+    --enable-shout \
+    --enable-matroska \
+    --enable-mod \
+    --enable-mpc \
+    --enable-mad \
+    --enable-mpg123 \
+    --enable-gst-decode \
+    --enable-avcodec \
+    --enable-libva \
+    --enable-avformat \
+    --enable-postproc \
+    --enable-faad \
+    --enable-vpx \
+    --enable-twolame \
+    --disable-fdkaac \
+    --enable-a52 \
+    --enable-dca \
+    --enable-flac \
+    --enable-libmpeg2 \
+    --enable-vorbis \
+    --enable-speex \
+    --enable-opus \
+    --enable-oggspots \
+    --disable-schroedinger \
+    --enable-png \
+    --enable-jpeg \
+    --enable-x264 \
+    --enable-x265 \
+    --enable-zvbi \
+    --enable-libass \
+    --enable-kate \
+    --enable-tiger \
+    --enable-vdpau \
+    --enable-wayland \
+    --enable-sdl-image \
+    --enable-freetype \
+    --enable-fribidi \
+    --enable-harfbuzz \
+    --enable-fontconfig \
+    --enable-svg \
+    --enable-svgdec \
+    --enable-aa \
+    --enable-caca \
+    --enable-pulse \
+    --enable-alsa \
+    --enable-jack \
+    --enable-samplerate \
+    --enable-soxr \
+    --enable-chromaprint \
+    --enable-chromecast \
+    --enable-qt \
+    --enable-skins2 \
+    --enable-libtar \
+    --enable-ncurses \
+    --enable-lirc \
+    --enable-goom \
+    --enable-projectm \
+    --enable-avahi \
+    --enable-mtp \
+    --enable-upnp \
+    --enable-microdns \
+    --enable-libxml2 \
+    --disable-libgcrypt \
+    --enable-gnutls \
+    --enable-taglib \
+    --enable-secret \
+    --enable-kwallet \
+    --disable-update-check \
+    --enable-notify \
+    --enable-libplacebo \
+    --enable-vlc \
+    --enable-aribsub \
+    --enable-aribcam \
+    --enable-aom \
+    --enable-srt \
+    --enable-dav1d
+  make
