3d5d4f8ade3d7f98b2289c9a6ca5727bbddc623e
Aylinux <cihanalk@gmail.com>
2021-03-06 02:58:23 +0300

    Update derle.sh

--- talimatlar/s/samba#4.13.4-1/derle.sh
+++ talimatlar/s/samba#4.13.4-1/derle.sh
@@ -1,21 +1,28 @@
-sed -r 's/nss_(setpw|endpw|setgr|endgr)ent/my_&/' \
-    -i nsswitch/nsstest.c
+_samba4_idmap_modules=idmap_ad,idmap_rid,idmap_adex,idmap_hash,idmap_tdb2
+_samba4_pdb_modules=pdb_tdbsam,pdb_ldap,pdb_ads,pdb_smbpasswd,pdb_wbc_sam,pdb_samba4
+_samba4_auth_modules=auth_unix,auth_wbc,auth_server,auth_netlogond,auth_script,auth_samba4
 
-echo "^samba4.rpc.echo.*on.*ncacn_np.*with.*object.*nt4_dc" >> selftest/knownfail
-
-CFLAGS+=" -I/usr/include/tirpc"         \
-LDFLAGS+=" -ltirpc"                     \
-./configure                             \
-    --prefix=/usr                       \
-    --sysconfdir=/etc                   \
-    --localstatedir=/var                \
-    --with-piddir=/run/samba            \
-    --with-pammodulesdir=/lib/security  \
-    --without-ad-dc                     \
-    --enable-fhs                        \
-    --bundled-libraries=!tdb            \
-                    --without-{ad-dc,ads,ldap} \
-                --without-json  \
-    --enable-selftest
+./configure --enable-fhs \
+--prefix=/usr \
+--sysconfdir=/etc \
+--sbindir=/usr/bin \
+--libdir=/usr/lib \
+--libexecdir=/usr/lib/samba \
+--localstatedir=/var \
+--with-configdir=/etc/samba \
+--with-lockdir=/var/cache/samba \
+--with-sockets-dir=/run/samba \
+--with-piddir=/run \
+--with-ads \
+--with-ldap \
+--with-winbind \
+--with-acl-support \
+--enable-gnutls \
+--enable-cups \
+--with-pam \
+--with-pammodulesdir=/usr/lib/security \
+--bundled-libraries=!tdb,!talloc,!pytalloc-util,!tevent,!popt,!ldb,!pyldb-util \
+--with-shared-modules=${_samba4_idmap_modules},${_samba4_pdb_modules},${_samba4_auth_modules} \
+--disable-rpath-install 
 
 make
