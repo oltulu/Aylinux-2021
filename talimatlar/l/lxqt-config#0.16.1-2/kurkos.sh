712cb8cf54c48cd6106ae0e07b51b49d65ec156f
Aylinux <cihanalk@gmail.com>
2021-03-10 22:31:13 +0300

    Update kurkos.sh

--- talimatlar/l/lxqt-config#0.16.1-2/kurkos.sh
+++ talimatlar/l/lxqt-config#0.16.1-2/kurkos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ -f /var/lib/mps/db/xf86-input-synaptics/kurulan ];then
-	mps sil xf86-input-synaptics --ona
-	echo "Dokunmatik fare ayarlarında oluşacak sorunu gidermek için xf86-input-synaptics paketi kaldırıldı"	
+	paket sil xf86-input-synaptics --ona
+	echo "Dokunmatik panel ayarlarında oluşacak sorunu gidermek için xf86-input-synaptics paketi kaldırıldı"	
 fi
