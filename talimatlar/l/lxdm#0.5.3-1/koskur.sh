3b1a13df9c12a2d3951e379033f84e269d66faad
Aylinux <cihanalk@gmail.com>
2021-03-13 00:58:28 +0300

    Create koskur.sh

---
+++ talimatlar/l/lxdm#0.5.3-1/koskur.sh
@@ -0,0 +1,19 @@
+if [ -f /etc/init.d/lightdm ];then
+	service stop lightdm
+	echo "lightdm servisinin kurulu olduğu algılandığı, Çakışmayı önlemek için durduruldu"
+	service remove lightdm
+	echo "lightdm servisi kaldırıldı"
+fi
+if [ -f /etc/init.d/sddm ];then
+	service stop sddm
+	echo "sddm servisinin kurulu olduğu algılandığı, Çakışmayı önlemek için durduruldu"
+	service remove sddm
+	echo "sddm servisi kaldırıldı"
+fi
+if [ -f /etc/init.d/slim ];then
+	service stop slim
+	echo "slim servisinin kurulu olduğu algılandığı, Çakışmayı önlemek için durduruldu"
+	service remove slim
+	echo "slim servisi kaldırıldı"
+fi
+echo "slim kurulum için hazır"
