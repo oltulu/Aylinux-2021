becba409a773ea627591de7d9c53df6fddb8782b
Aylinux <cihanalk@gmail.com>
2021-03-13 12:24:34 +0300

    Update derle.sh

--- talimatlar/k/kbd#2.4.0-1/derle.sh
+++ talimatlar/k/kbd#2.4.0-1/derle.sh
@@ -6,7 +6,7 @@ mv data/keymaps/i386/olpc/pt{,-olpc}.map
 mv data/keymaps/i386/fgGIod/trf{,-fgGIod}.map
 mv data/keymaps/i386/colemak/{en-latin9,colemak}.map
 
-patch -Np1 -i ../fix-euro2.patch
+#patch -Np1 -i ../fix-euro2.patch
 patch -Np1 -i /sources/kbd-2.4.0-backspace-1.patch
 ./configure --prefix=/usr --datadir=/usr/share/kbd --disable-vlock --disable-tests
 make 
