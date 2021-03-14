97bad13da64186ac3e326e2816ad1c62c252691c
Aylinux <cihanalk@gmail.com>
2021-03-14 20:33:52 +0300

    Update derle.sh

--- talimatlar/g/glibc#2.33-1/derle.sh
+++ talimatlar/g/glibc#2.33-1/derle.sh
@@ -1,33 +1,62 @@
- cd "$SRC/glibc-build"
+cd $SRC/linux-5.9.8
+make mrproper
+make headers_check
+make INSTALL_HDR_PATH=dest headers_install
+find dest/include \( -name .install -o -name ..install.cmd \) -delete
+mkdir -p $PKG/usr/include
+cp -rv dest/include/* $PKG/usr/include
 
-  echo "slibdir=/usr/lib" >> configparms
-  echo "rtlddir=/usr/lib" >> configparms
-  echo "sbindir=/usr/bin" >> configparms
-  echo "rootsbindir=/usr/bin" >> configparms
+rm -rf $SRC/linux-5.9.8
+cd $SRC/glibc-$surum
+patch -Np1 -i $SRC/glibc-$surum-fhs-1.patch || exit 1
 
-  # remove fortify for building libraries
-  CPPFLAGS=${CPPFLAGS/-D_FORTIFY_SOURCE=2/}
+# glibc'in son halinde tools a link olmaması için
+ln -sfv /tools/lib/gcc /usr/lib
 
-  #
-  CFLAGS=${CFLAGS/-fno-plt/}
-  CXXFLAGS=${CXXFLAGS/-fno-plt/}
-  LDFLAGS=${LDFLAGS/,-z,now/}
+#[ -f /usr/include/limits.h ] && rm -f /usr/include/limits.h
 
-  "$SRC/glibc/configure" \
-      --libdir=/usr/lib \
-      --libexecdir=/usr/lib \
-      --prefix=/usr --with-headers=/usr/include --enable-add-ons --enable-bind-now --enable-cet --enable-lock-elision --enable-multi-arch --enable-stack-protector=strong --enable-stackguard-randomization --enable-static-pie --enable-systemtap --disable-profile --disable-werror
+# 64bit için gerekli kısayollar
+mkdir $PKG/usr/lib
 
-  # build libraries with fortify disabled
-  echo "build-programs=no" >> configparms
-  make
+GCC_INCDIR=/usr/lib/gcc/x86_64-pc-linux-gnu/10.2.0/include
+ln -s ../lib/ld-linux-x86-64.so.2 $PKG/usr/lib
+ln -s ../lib/ld-linux-x86-64.so.2 $PKG/usr/lib/ld-lsb-x86-64.so.3
 
-  # re-enable fortify for programs
-  sed -i "/build-programs=/s#no#yes#" configparms
+mkdir -v build
+cd       build
 
-  echo "CC += -D_FORTIFY_SOURCE=2" >> configparms
-  echo "CXX += -D_FORTIFY_SOURCE=2" >> configparms
-  make
+echo "slibdir=/usr/lib" >> configparms
+echo "rtlddir=/usr/lib" >> configparms
+echo "sbindir=/usr/bin" >> configparms
+echo "rootsbindir=/usr/bin" >> configparms
 
-  # build info pages manually for reprducibility
-  make info
+# remove stack protector for libs
+CFLAGS=${CFLAGS/-fstack-protector/}
+CPPFLAGS=${CPPFLAGS/-D_FORTIFY_SOURCE=2/}
+
+#CC="gcc -ffile-prefix-map=/tools=/usr" \
+
+CC="gcc -isystem $GCC_INCDIR -isystem /usr/include"  \
+${SRC}/glibc-${surum}/configure --prefix=/usr \
+--libdir=/usr/lib --libexecdir=/usr/lib \
+--with-bugurl=https://milislinux.org \
+--disable-werror                       \
+--enable-kernel=3.2                    \
+--enable-stack-protector=strong  \
+--disable-timezone-tools
+
+# build libs without stack protector
+echo "build-programs=no" >> configparms
+make
+
+# build bins with stack protector
+sed -i "/build-programs=/s#no#yes#" configparms
+echo "CC += -fstack-protector -D_FORTIFY_SOURCE=2" >> configparms
+echo "CXX += -fstack-protector -D_FORTIFY_SOURCE=2" >> configparms
+make
+
+# no stack protector for running tests
+sed -i '5,7d' configparms
+sed -i '/FORTIFY/d' configparms
+
+unset GCC_INCDIR
