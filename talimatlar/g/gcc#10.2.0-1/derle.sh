f716f06ce636d692d7e9db1066163ea5ef27058c
Aylinux <cihanalk@gmail.com>
2021-03-14 20:21:20 +0300

    Update derle.sh

--- talimatlar/g/gcc#10.2.0-1/derle.sh
+++ talimatlar/g/gcc#10.2.0-1/derle.sh
@@ -1,19 +1,19 @@
-    [ -f /tools/bin/ld ] && mv /tools/bin/ld /tools/bin/ld.eski
-    sed -e '/m64=/s/lib64/lib/' \
-    -i.orig gcc/config/i386/t-linux64
+[ -f /tools/bin/ld ] && mv /tools/bin/ld /tools/bin/ld.eski
+sed -e '/m64=/s/lib64/lib/' \
+-i.orig gcc/config/i386/t-linux64
 
-    [ -L /usr/lib/gcc ] && rm -f /usr/lib/gcc
+[ -L /usr/lib/gcc ] && rm -f /usr/lib/gcc
 
-    mkdir -v build
-    cd build
+mkdir -v build
+cd build
 
-    SED=sed \
-    ../configure --prefix=/usr \
-    --libdir=/usr/lib \
-    --libexecdir=/usr/lib \
-    --enable-languages=c,c++,fortran,objc \
-    --disable-multilib \
-    --disable-bootstrap \
-    --with-system-zlib
+SED=sed \
+../configure --prefix=/usr \
+--libdir=/usr/lib \
+--libexecdir=/usr/lib \
+--enable-languages=c,c++,fortran,objc \
+--disable-multilib \
+--disable-bootstrap \
+--with-system-zlib
 
-    make
+make
