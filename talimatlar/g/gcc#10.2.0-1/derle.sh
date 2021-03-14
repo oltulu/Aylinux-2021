ba4eb38133d9af7461fda98121771ed3429a11e8
Aylinux <cihanalk@gmail.com>
2021-03-14 20:20:34 +0300

    Update derle.sh

--- talimatlar/g/gcc#10.2.0-1/derle.sh
+++ talimatlar/g/gcc#10.2.0-1/derle.sh
@@ -1,41 +1,19 @@
-ln -s ../isl-0.21 isl
+    [ -f /tools/bin/ld ] && mv /tools/bin/ld /tools/bin/ld.eski
+    sed -e '/m64=/s/lib64/lib/' \
+    -i.orig gcc/config/i386/t-linux64
 
-  # Do not run fixincludes
-  sed -i 's@\./fixinc\.sh@-c true@' gcc/Makefile.in
+    [ -L /usr/lib/gcc ] && rm -f /usr/lib/gcc
 
-  # Arch Linux installs x86_64 libraries /lib
-  sed -i '/m64=/s/lib64/lib/' gcc/config/i386/t-linux64
+    mkdir -v build
+    cd build
 
-  # hack! - some configure tests for header files using "$CPP $CPPFLAGS"
-  sed -i "/ac_cpp=/s/\$CPPFLAGS/\$CPPFLAGS -O2/" {libiberty,gcc}/configure
-
-  # D hacks
-  patch -p1 -i "/sources/gdc_phobos_path.patch"
-
-  # Turn off SSP for nostdlib|nodefaultlibs|ffreestanding
-  # https://bugs.archlinux.org/task/64270
-  patch -p1 -i "/sources/fs64270.patch"
-
-  # Fix a crash in mpv when mesa 20.2 is compiled with LTO
-  # https://gitlab.freedesktop.org/mesa/mesa/-/issues/3239
-  # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=96482
-  patch -Np1 -i /sources/ipa-fix-bit-CPP-when-combined-with-IPA-bit-CP.patch
-  patch -Np1 -i /sources/ipa-fix-ICE-in-get_default_value.patch
-
-  mkdir -p "$SRC/gcc-build"
- cd $SRC/gcc-build
-
-  # using -pipe causes spurious test-suite failures
-  # http://gcc.gnu.org/bugzilla/show_bug.cgi?id=48565
-  CFLAGS=${CFLAGS/-pipe/}
-  CXXFLAGS=${CXXFLAGS/-pipe/}
-
-  $SRC/gcc-$surum/configure --prefix=/usr \
+    SED=sed \
+    ../configure --prefix=/usr \
     --libdir=/usr/lib \
-    --enable-languages=c,c++,objc,fortran \
+    --libexecdir=/usr/lib \
+    --enable-languages=c,c++,fortran,objc \
     --disable-multilib \
     --disable-bootstrap \
     --with-system-zlib
 
-  make
-
+    make
