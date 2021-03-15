cf4d0941204dc444397d18dcb7ea010ba9566dd5
Aylinux <cihanalk@gmail.com>
2021-03-16 00:09:18 +0300

    Update kurkos.sh

--- talimatlar/s/sddm#0.19.0-1/kurkos.sh
+++ talimatlar/s/sddm#0.19.0-1/kurkos.sh
@@ -1,3 +1,3 @@
 #. /lib/lsb/init-functions
 sed -i "s/^id:[3-5]/id:5/" /etc/inittab
-echo  "Init default set to 5 in the /etc/inittab file"
+echo  "/etc/inittab dosyasında varsayılan init 5 olarak ayarlandı"
