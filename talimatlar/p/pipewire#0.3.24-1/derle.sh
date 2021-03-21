9c002fa8807af8b7673ec496ddfa08aa65a0380f
Aylinux <cihanalk@gmail.com>
2021-03-21 19:29:18 +0300

    Update derle.sh

--- talimatlar/p/pipewire#0.3.24-1/derle.sh
+++ talimatlar/p/pipewire#0.3.24-1/derle.sh
@@ -1,8 +1,17 @@
-cd $SRC
+cd $isim-$surum
 [[ -e /usr/lib/pkgconfig/jack.pc ]] || PKGMK_PW+=' -Dpipewire-jack=disabled -Djack=disabled'
   [[ -e /usr/lib/pkgconfig/vulkan.pc ]] || PKGMK_PW+=' -Dpipewire-vulkan=disabled -Dvulkan=disabled'
   [[ -e /usr/lib/pkgconfig/bluez.pc ]] || PKGMK_PW+=' -Dbluez5=disabled'
   [[ -e /usr/lib/pkgconfig/libv4l2.pc ]] || PKGMK_PW+=' -Dv4l2=disabled'
   [[ -e /usr/include/pulse/glib-mainloop.h ]] || PKGMK_PW+=' -Dpipewire-pulseaudio=disabled'
   [[ -e /usr/lib/pkgconfig/gstreamer-1.0.pc ]] || PKGMK_PW+=' -Dgstreamer=disabled'
+cd $SRC  
+    milis-meson setup $isim-$surum build $PKGMK_PW \
+    -D prefix=/usr \
+    --buildtype=plain \
+    -D docs=disabled \
+    -D tests=disabled
+ 
+
+  meson compile -C build
 
