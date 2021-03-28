b8bc1d3fcff5aaa79740dfb6861fc7133d6be024
Aylinux <cihanalk@gmail.com>
2021-03-28 19:44:20 +0300

    Update derle.sh

--- talimatlar/a/apr#1.7.0-1/derle.sh
+++ talimatlar/a/apr#1.7.0-1/derle.sh
@@ -3,6 +3,7 @@ patch -Np1 -i ../ship_find_apr.m4.patch
 patch -Np1 -i ../fix-apr.pc.patch
 patch -Np1 -i ../omit_extra_libs.patch
 patch -Np1 -i ../dont_override_external_buildflags
+patch -Np1 -i /sources/atomic64.patch
 sed -e 's/env python/env python2/' -i build/gen-build.py
 sed -e 's/python/python2/g' -i build/buildcheck.sh
 
