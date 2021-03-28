225ca834370713dc2bd617c6aef8b226602eb8c9
Aylinux <cihanalk@gmail.com>
2021-03-28 19:47:38 +0300

    Update derle.sh

--- talimatlar/a/apr#1.7.0-1/derle.sh
+++ talimatlar/a/apr#1.7.0-1/derle.sh
@@ -1,17 +1,15 @@
-patch -Np1 -i ../fix_apr-config.patch
-patch -Np1 -i ../ship_find_apr.m4.patch
-patch -Np1 -i ../fix-apr.pc.patch
-patch -Np1 -i ../omit_extra_libs.patch
-patch -Np1 -i ../dont_override_external_buildflags
-patch -Np1 -i /sources/atomic64.patch
-sed -e 's/env python/env python2/' -i build/gen-build.py
-sed -e 's/python/python2/g' -i build/buildcheck.sh
+ ./configure --prefix=/usr \
+                --with-installbuilddir=/usr/share/apr-1 
+    make 
+    make -j1 DESTDIR=$PKG install
 
-./buildconf
-
-./configure --prefix=/usr --includedir=/usr/include/apr-1 \
---with-installbuilddir=/usr/share/apr-1/build \
---enable-nonportable-atomics \
---with-devrandom=/dev/urandom --disable-static
-
-make
+    cd $SRC/$isim-util-1.6.1
+    ./configure --prefix=/usr \
+                --with-apr=$SRC/$isim-$surum \
+                --with-expat=/usr \
+                --with-berkeley-db=/usr \
+                --with-gdbm=/usr \
+                --without-sqlite3 \
+                --disable-util-dso
+    make
+    make DESTDIR=$PKG install
