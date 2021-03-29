55cbf4f94996fbde253e0378c42d8be839c9c408
Aylinux <cihanalk@gmail.com>
2021-03-30 00:18:32 +0300

    Update pakur.sh

--- talimatlar/a/ardublock#1026-1/pakur.sh
+++ talimatlar/a/ardublock#1026-1/pakur.sh
@@ -4,7 +4,7 @@ cat > $PKG/usr/share/applications/ardunoblock.desktop << "EOf"
 [Desktop Entry]
 Name=Ardunoblock
 Comment=Robotik kodlama
-Exec=java -jar /usr/share/arduino/tools/ArduBlockTool/tool/ardublock-all.jarl.jar
+Exec=java -jar /usr/share/arduino/tools/ArduBlockTool/tool/ardublock-all.jar
 Icon=assistant
 Terminal=false
 Encoding=UTf-8
