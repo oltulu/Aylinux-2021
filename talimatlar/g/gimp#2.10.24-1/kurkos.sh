7c35b14d3f08afa4d8c4439f4c07b767bb642beb
Aylinux <cihanalk@gmail.com>
2021-04-01 11:44:06 +0300

    Update and rename kurkos.sh to pakur.sh

--- talimatlar/g/gimp#2.10.24-1/kurkos.sh
+++
@@ -1,7 +0,0 @@
-cat > /usr/share/applications/gimp-screenshot-plugin.desktop << EOF 
-[Desktop Entry] Type=Application 
-Name=GIMP Screenshot Plugin 
-Exec=/usr/lib/gimp/2.0/plug-ins/screenshot/screenshot 
-NoDisplay=true 
-X-KDE-DBUS-Restricted-Interfaces=org.kde.kwin.Screenshot 
-EOF
---
+++ talimatlar/g/gimp#2.10.24-1/pakur.sh
@@ -0,0 +1,13 @@
+install -D -m644 "/sources/linux.gpl" "${PKG}/usr/share/gimp/2.0/palettes/Linux.gpl"
+ln -s gimptool-2.0 "${PKG}/usr/bin/gimptool"
+rm $PKG/usr/share/gimp/2.0/images/gimp-splash.png
+cp /sources/gimp-splash.png $PKG/usr/share/gimp/2.0/images/gimp-splash.png
+
+
+cat > /usr/share/applications/gimp-screenshot-plugin.desktop << EOF 
+[Desktop Entry] Type=Application 
+Name=GIMP ekran resmi eklentisi
+Exec=/usr/lib/gimp/2.0/plug-ins/screenshot/screenshot 
+NoDisplay=true 
+X-KDE-DBUS-Restricted-Interfaces=org.kde.kwin.Screenshot 
+EOF
