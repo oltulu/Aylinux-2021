1c7f25008d127135be2be64753ddd79fadae78c4
Aylinux <cihanalk@gmail.com>
2021-04-06 22:32:05 +0300

    Add files via upload

---
+++ talimatlar/g/garuda-boot-repair#0.1-1/garuda-boot-repair_tr.ts
@@ -0,0 +1,352 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="20"/>
+        <source>Garuda Boot Repair</source>
+        <translation>Garuda Önyükleme Onarımı</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="39"/>
+        <source>Garuda Boot Repair is a utility that can be used to reinstall GRUB bootloader on the ESP (EFI System Partition), MBR (Master Boot Record) or root partition. It provides the option to reconstruct the GRUB configuration file and to back up and restore MBR or PBR (root).</source>
+        <translation>Garuda Önyükleme Onarımı, GRUB önyükleyicisini ESP&apos;ye (EFI Sistem Bölümü), MBR&apos;ye (Ana Önyükleme Kaydı) veya kök bölüme yeniden yüklemek için kullanılabilecek bir programdır. GRUB yapılandırma dosyasını yeniden oluşturma ve MBR&apos;yi veya PBR&apos;yi (kök) yedekleme ve geri yükleme seçeneği sunar.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="58"/>
+        <source>What would you like to do?</source>
+        <translation>Ne yapmak istersin?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="79"/>
+        <source>Backup MBR or PBR (legacy boot only)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="86"/>
+        <source>Reinstall GRUB bootloader on ESP, MBR or PBR (root)</source>
+        <translation>GRUB önyükleyiciyi ESP, MBR veya PBR&apos;ye (root) yeniden kur</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="96"/>
+        <source>Repair GRUB configuration file</source>
+        <translation>GRUB yapılandırma dosyasını onar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="103"/>
+        <source>Restore MBR or PBR from backup (legacy boot only)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="136"/>
+        <location filename="../mainwindow.cpp" line="485"/>
+        <source>Select Boot Method</source>
+        <translation>Önyükleme Yöntemini Seçin</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="154"/>
+        <source>Master Boot Record</source>
+        <translation>Ana Önyükleme Kaydı</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="157"/>
+        <source>MBR</source>
+        <translation>MBR</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="160"/>
+        <location filename="../mainwindow.ui" line="430"/>
+        <source>Alt+B</source>
+        <translation>Alt+B</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="182"/>
+        <source>Root (Partition Boot Record)</source>
+        <translation>Root (Bölüm Önyükleme Kaydı)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="185"/>
+        <location filename="../mainwindow.cpp" line="488"/>
+        <source>root</source>
+        <translation>Kök</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="236"/>
+        <location filename="../mainwindow.cpp" line="487"/>
+        <source>Install on:</source>
+        <translation>Buraya kur:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="255"/>
+        <location filename="../mainwindow.cpp" line="486"/>
+        <source>Location:</source>
+        <translation>Konum:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="277"/>
+        <location filename="../mainwindow.cpp" line="496"/>
+        <source>Select root location:</source>
+        <translation>root konumunu seçin</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="290"/>
+        <source>EFI System Partition</source>
+        <translation>EFİ Sistem Bölümü</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="293"/>
+        <source>ESP</source>
+        <translation>ESP</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="420"/>
+        <source>About this application</source>
+        <translation>Uygulama hakkında</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="423"/>
+        <source>About...</source>
+        <translation>Hakkında...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="472"/>
+        <source>Display help </source>
+        <translation>Yardımı görüntüle</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="475"/>
+        <source>Help</source>
+        <translation>Yardım</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="482"/>
+        <source>Alt+H</source>
+        <translation>Alt+H</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="523"/>
+        <source>Cancel any changes then quit</source>
+        <translation>Değişiklikleri iptal et ve çık</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="526"/>
+        <source>Cancel</source>
+        <translation>İptal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="533"/>
+        <source>Alt+N</source>
+        <translation>Alt+N</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="552"/>
+        <source>Apply any changes</source>
+        <translation>Değişiklikleri uygula</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="555"/>
+        <location filename="../mainwindow.cpp" line="70"/>
+        <source>Apply</source>
+        <translation>Uygula</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="86"/>
+        <source>GRUB is being installed on %1 device.</source>
+        <translation>GRUB %1 cihazına kuruluyor.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="118"/>
+        <location filename="../mainwindow.cpp" line="203"/>
+        <location filename="../mainwindow.cpp" line="312"/>
+        <location filename="../mainwindow.cpp" line="394"/>
+        <location filename="../mainwindow.cpp" line="419"/>
+        <location filename="../mainwindow.cpp" line="527"/>
+        <location filename="../mainwindow.cpp" line="534"/>
+        <location filename="../mainwindow.cpp" line="597"/>
+        <source>Error</source>
+        <translation>Hata</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="119"/>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>Could not set up chroot environment.
+Please double-check the selected location.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="179"/>
+        <source>The GRUB configuration file (grub.cfg) is being rebuilt.</source>
+        <translation>GRUB yapılandırma dosyası (grub.cfg) yeniden oluşturuluyor.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="222"/>
+        <source>Backing up MBR or PBR from %1 device.</source>
+        <translation>%1 cihazından MBR veya PBR yedekleniyor.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="287"/>
+        <source>Warning</source>
+        <translation>Uyarı</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="288"/>
+        <source>You are going to write the content of </source>
+        <translation>...&apos;ın içeriğini yazacaksınız</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="288"/>
+        <source> to </source>
+        <translation>&apos;e</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="288"/>
+        <source>
+
+Are you sure?</source>
+        <translation>
+
+Emin misiniz?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="293"/>
+        <source>Restoring MBR/PBR from backup to %1 device.</source>
+        <translation>MBR/PBR yedekten %1 cihazına kuruluyor.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="313"/>
+        <source>Could not find EFI system partition (ESP) on any system disks. Please create an ESP and try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="348"/>
+        <source>Select %1 location:</source>
+        <translation>%1 konumunu seçin:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="372"/>
+        <source>Back</source>
+        <translation>Geri</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="388"/>
+        <source>Success</source>
+        <translation>Başarılı</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="389"/>
+        <source>Process finished with success.&lt;p&gt;&lt;b&gt;Do you want to exit Garuda Boot Repair?&lt;/b&gt;</source>
+        <translation>İşlem başarıyla sona erdi. &lt;p&gt;&lt;b&gt;Garuda Önyükleme Onarımı&apos;ndan çıkmak ister misiniz?&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="395"/>
+        <source>Process finished. Errors have occurred.</source>
+        <translation>Süreç tamamlandı. Bir hata oluştu.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="412"/>
+        <source>Enter password to unlock %1 encrypted partition:</source>
+        <translation>%1 şifreli bölümün kilidini açmak için şifreyi girin:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="420"/>
+        <source>Sorry, could not open %1 LUKS container</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="495"/>
+        <source>Select GRUB location</source>
+        <translation>GRUB konumunu seçin</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="507"/>
+        <source>Select Item to Back Up</source>
+        <translation>Yedeklenecek Öğeyi Seçin</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="514"/>
+        <source>Select Item to Restore</source>
+        <translation>Geri Yüklenecek Öğeyi Seçin</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="525"/>
+        <source>Select backup file name</source>
+        <translation>Yedekleme dosyası adı seçimi</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="527"/>
+        <location filename="../mainwindow.cpp" line="534"/>
+        <source>No file was selected.</source>
+        <translation>Hiçbir dosya seçilmedi.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="532"/>
+        <source>Select MBR or PBR backup file</source>
+        <translation>MBR veya PBR yedek dosyasını seçin</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="552"/>
+        <source>About %1</source>
+        <translation>Hakkında %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="553"/>
+        <source>Version: </source>
+        <translation>Sürüm:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="554"/>
+        <source>Simple boot repair program for Garuda Linux</source>
+        <translation>Garuda Linux için basit bir önyükleme onarma programı</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="556"/>
+        <source>Copyright (c) Garuda Linux</source>
+        <translation>Telif Hakkı (c) Garuda Linux</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="557"/>
+        <source>%1 License</source>
+        <translation>%1 Telif Hakkı</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="571"/>
+        <source>%1 Help</source>
+        <translation>%1 Yardım</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="598"/>
+        <source>Sorry, could not mount %1 partition</source>
+        <translation>% 1 bölümü maalesef bağlanamadı</translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <location filename="../about.cpp" line="32"/>
+        <source>License</source>
+        <translation>Ruhsat</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="33"/>
+        <location filename="../about.cpp" line="43"/>
+        <source>Changelog</source>
+        <translation>Değişim günlüğü</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="34"/>
+        <source>Cancel</source>
+        <translation>İptal</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="51"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Kapat</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="64"/>
+        <source>You must run this program as root.</source>
+        <translation>Bu programı root olarak çalıştırmalısınız.</translation>
+    </message>
+</context>
+</TS>
