1754de6ffd5d43fd2736d709173744cbe75bcc13
Aylinux <cihanalk@gmail.com>
2021-04-06 16:00:48 +0300

    Update derle.sh

--- talimatlar/l/libvirt#7.1.0-1/derle.sh
+++ talimatlar/l/libvirt#7.1.0-1/derle.sh
@@ -1,22 +1,35 @@
-autoreconf -fi
+ sed -i 's|/sysconfig/|/conf.d/|g' \
+    src/remote/libvirtd.service.in \
+    tools/{libvirt-guests.service,libvirt-guests.sh,virt-pki-validate}.in \
+    src/locking/virtlockd.service.in \
+    src/logging/virtlogd.service.in
+  sed -i 's|/usr/libexec/qemu-bridge-helper|/usr/lib/qemu/qemu-bridge-helper|g' \
+    src/qemu/qemu.conf \
+    src/qemu/test_libvirtd_qemu.aug.in
 
-mkdir -p build ; cd build
-export PYTHON=$(command -v python)
-export LDFLAGS="$LDFLAGS -lX11"
-../configure --prefix=/usr \
---libexec=/usr/lib/libvirt \
---sbindir=/usr/bin \
---without-hal \
---with-storage-lvm \
---with-qemu \
---with-qemu-user=nobody \
---with-qemu-group=kvm \
---without-netcf \
---with-interface \
---with-udev \
---without-apparmor \
---with-qemu  \
---disable-static \
---without-wireshark-dissector
+  sed -i 's/notify/simple/' src/remote/libvirtd.service.in
 
-make
+  milis-meson build \
+    --libexecdir=lib/libvirt \
+    --native-file "$srcdir"/find_programs.ini \
+    -Drunstatedir=/run \
+    -Dqemu_group=kvm \
+    -Dnetcf=disabled \
+    -Dopenwsman=disabled \
+    -Dapparmor=disabled \
+    -Dselinux=disabled \
+    -Dwireshark_dissector=disabled \
+    -Ddriver_bhyve=disabled \
+    -Ddriver_hyperv=disabled \
+    -Ddriver_libxl=disabled \
+    -Ddriver_vz=disabled \
+    -Dsecdriver_apparmor=disabled \
+    -Dsecdriver_selinux=disabled \
+    -Dstorage_sheepdog=disabled \
+    -Dstorage_vstorage=disabled \
+    -Ddtrace=disabled \
+    -Dnumad=disabled \
+    -Dstorage_zfs=enabled \
+    -Dstorage_rbd=enabled
+
+  ninja -C build
