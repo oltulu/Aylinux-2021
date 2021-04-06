72e1dd2ea1623ad46ec330032d1ed5e7ec5be374
Aylinux <cihanalk@gmail.com>
2021-04-06 23:09:48 +0300

    Update derle.sh

--- talimatlar/b/blender#2.91.0-1/derle.sh
+++ talimatlar/b/blender#2.91.0-1/derle.sh
@@ -1,31 +1,19 @@
-export py_version="3.7"
-mkdir build
-cd build
-cmake \
--C../build_files/cmake/config/blender_release.cmake .. \
--DCMAKE_INSTALL_PREFIX=/usr \
--DCMAKE_BUILD_TYPE=Release \
--DWITH_INSTALL_PORTABLE=OFF \
--DWITH_PYTHON_INSTALL=OFF \
--DWITH_JACK=ON \
--DWITH_CODEC_FFMPEG=ON \
--DWITH_CODEC_SNDFILE=ON \
--DWITH_OPENMP=ON \
--DWITH_FFTW3=ON \
--DWITH_MOD_OCEANSIM=ON \
--DWITH_CYCLES=ON \
--DWITH_OPENCOLORIO=ON \
--DWITH_IMAGE_OPENEXR=ON \
--DWITH_IMAGE_OPENJPEG=ON \
--DWITH_OPENSUBDIV=ON \
--DWITH_OPENCOLLADA=ON \
--DWITH_SYSTEM_GLEW=ON \
--DWITH_OPENVDB=ON \
--DWITH_BUILDINFO=OFF \
--DSTOP_ON_WARNING=0 \
--DPYTHON_VERSION=${py_version} \
--DPYTHON_LIBPATH=/usr/lib \
--DPYTHON_LIBRARY=python${py_version}m \
--DPYTHON_INCLUDE_DIRS="/usr/include/python${py_version}m" || make $MAKEJOBS
+cd $SRC
+cmake -S "${isim}"-"${surum}" -B build -G Ninja     \
+        -D WITH_CYCLES=OFF                              \
+        -D PYTHON_VERSION=3.9                           \
+        -D WITH_CYCLES_EMBREE=OFF                       \
+        -D WITH_INTERNATIONAL=OFF                       \
+        -D WITH_PYTHON_INSTALL=OFF                      \
+        -D PYTHON_LIBPATH=/usr/lib                      \
+        -D CMAKE_BUILD_TYPE=Release                     \
+        -D PYTHON_LIBRARY=python3.9                     \
+        -D WITH_DOC_MANPAGE=ON                          \
+        -D CMAKE_INSTALL_PREFIX=/usr                    \
+        -D WITH_INSTALL_PORTABLE=OFF                    \
+        -D OPTIX_ROOT_DIR="${SRC}"/include              \
+        -D CMAKE_C_FLAGS_RELEASE="$CFLAGS -DNDEBUG"     \
+        -D PYTHON_INCLUDE_DIRS=/usr/include/python3.9   \
+        -D CMAKE_CXX_FLAGS_RELEASE="$CXXFLAGS -DNDEBUG"
 
 
